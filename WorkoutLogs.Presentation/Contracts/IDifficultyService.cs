fdb1e7759de2d4b7a2fd9cfec3a79d8cb0a643fa
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-03-04 14:36:20 -0800

    DifficultyService

---
+++ WorkoutLogs.Presentation/Contracts/IDifficultyService.cs
@@ -0,0 +1,9 @@
+﻿using WorkoutLogs.Presentation.Services.Base;
+
+namespace WorkoutLogs.Presentation.Contracts
+{
+    public interface IDifficultyService
+    {
+        public Task<ICollection<DifficultyDto>> GetAllDifficulties();
+    }
+}
---
+++ WorkoutLogs.Presentation/Services/DifficultyService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using WorkoutLogs.Presentation.Contracts;
+using WorkoutLogs.Presentation.Services.Base;
+
+namespace WorkoutLogs.Presentation.Services
+{
+    public class DifficultyService : BaseHttpService, IDifficultyService
+    {
+        private readonly IMapper mapper;
+
+        public DifficultyService(IClient client, IMapper mapper) : base(client)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<ICollection<DifficultyDto>> GetAllDifficulties()
+        {
+            return await _client.DifficultyAllAsync();
+        }
+    }
+}
