01b3c0f5e57ce7617832245ba170a75007c5ad44
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2024-03-04 14:22:06 -0800

    ExerciseTypeService

--- WorkoutLogs.Presentation/Contracts/IExerciseTypeService.cs
+++ WorkoutLogs.Presentation/Contracts/IExerciseTypeService.cs
@@ -1,6 +1,10 @@
-﻿namespace WorkoutLogs.Presentation.Contracts
+﻿
+using WorkoutLogs.Presentation.Services.Base;
+
+namespace WorkoutLogs.Presentation.Contracts
 {
     public interface IExerciseTypeService
     {
+        public Task<ICollection<ExerciseTypeDto>> GetAllExerciseTypes();
     }
 }
---
+++ WorkoutLogs.Presentation/Services/ExerciseTypeService.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using WorkoutLogs.Presentation.Contracts;
+using WorkoutLogs.Presentation.Services.Base;
+
+namespace WorkoutLogs.Presentation.Services
+{
+    public class ExerciseTypeService : BaseHttpService, IExerciseTypeService
+    {
+        private readonly IMapper mapper;
+
+        public ExerciseTypeService(IClient client, IMapper mapper) : base(client)
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<ICollection<ExerciseTypeDto>> GetAllExerciseTypes()
+        {
+            return await _client.GetAllExerciseTypesAsync(new GetAllExerciseTypesQuery());
+        }
+    }
+}
